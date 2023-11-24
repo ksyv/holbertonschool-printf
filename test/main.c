973be8df3c49a312134b9cc5c3502bff17e7bdf9
ksyv <viais.kevin@hotmail.fr>
2023-11-24 09:37:42 +0100

    add description of test

--- test/main.c
+++ test/main.c
@@ -59,7 +59,7 @@ int main(void)
 	 commodo consequat. Duis aute irure dolor in reprehenderit in voluptate \
 	  velit esse cillum dolore eu fugiat nulla pariatur. \
 	  Excepteur sint occaecat cupidatat non proident, \
-	  sunt in culpa qui officia deserunt mollit anim id est laborum.");
+	  sunt in culpa qui officia deserunt mollit anim id est laborum."); /*Test a long text*/
 	printf("%s\n", "Lorem ipsum dolor sit amet,\
 	 consectetur adipiscing elit, sed do eiusmod tempor incididunt \
 	 ut labore et dolore magna aliqua. Ut enim ad minim veniam, \
